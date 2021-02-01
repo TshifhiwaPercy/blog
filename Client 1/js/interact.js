8298a96aa6475b3dfe19a68647bcbeeddb673ff2
Tshifhiwa <tshifhiwamphoshomali@gmail.com>
2021-02-01 17:09:38 +0200

    Version 1.0.0

---
+++ Client 1/js/interact.js
@@ -0,0 +1,7 @@
+var ncomment = document.getElementsByClassName('comment');
+var nlike = document.getElementsByClassName('like');
+var nviews = document.getElementsByClassName('views');
+var nshares = document.getElementsByClassName('share');
+ 
+
+
